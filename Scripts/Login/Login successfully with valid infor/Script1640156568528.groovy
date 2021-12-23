1e43b538c34a6aeeb856b6d952a24f880ea6c4ca
Như <truonghuynhnhu1412@gmail.com>
2021-12-23 09:56:44 +0700

    change code

--- Scripts/Login/Login successfully with valid infor/Script1640156568528.groovy
+++ Scripts/Login/Login successfully with valid infor/Script1640156568528.groovy
@@ -25,7 +25,5 @@ WebUI.setText(findTestObject('Page_Login/input_Sign up_password'), 'TestOps!2021
 
 WebUI.click(findTestObject('Page_Login/button_Sign in'))
 
-WebUI.verifyTextPresent('Welcome to', true)
-
-WebUI.closeBrowser()
+WebUI.verifyTextPresent('welcome', true)
 
